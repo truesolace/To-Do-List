15951aaa001f289cf902b2846a6df89b22c64027
truesolace <dallasrollyson@icloud.com>
2025-09-03 17:27:21 -0400

    Implemented a task class with getters and setters

--- todomain.js
+++ todomain.js
@@ -1,1 +1,38 @@
 console.log("Test");
+
+class Task {
+  name = "";
+  isComplete = false;
+  description;
+  dueDate;
+
+  constructor(inputName) {
+    this.name = inputName;
+  }
+
+  getName() {
+    return this.name;
+  }
+  getIsComplete() {
+    return this.isComplete;
+  }
+  getDescription() {
+    return this.description;
+  }
+  getDueDate() {
+    return this.dueDate;
+  }
+
+  setName(inputName) {
+    this.name = inputName;
+  }
+  setIsComplete(inputBool) {
+    this.isComplete = inputBool;
+  }
+  setDescription(inputDesc) {
+    this.description = inputDesc;
+  }
+  setDueDate(inputDate) {
+    this.dueDate = inputDate;
+  }
+}
