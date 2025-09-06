472c905b2307bd9559f7167f68976ba0c1e356da
truesolace <dallasrollyson@icloud.com>
2025-09-05 21:52:27 -0400

    Tested implementing the new Task class

--- ToDoMain.java
+++ ToDoMain.java
@@ -1,9 +1,8 @@
-
-
 class ToDoMain {
-	public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Task task = new Task("Temporary name");
 
-
-	}
+        System.out.println(task.getName());
+    }
 }
