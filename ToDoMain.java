13a4fcf024543b8896c14c1f0772a1be617f171c
truesolace <dallasrollyson@icloud.com>
2025-09-06 21:07:05 -0400

    Added user input functionality

--- ToDoMain.java
+++ ToDoMain.java
@@ -1,8 +1,15 @@
 class ToDoMain {
 
     public static void main(String[] args) {
-        Task task = new Task("Temporary name");
+        System.out.println("What would you like to name your task?");
+        String taskName;
+        try (Scanner scn = new Scanner(System.in)) {
+            taskName = scn.nextLine();
+        }
 
+        Task task = new Task(taskName);
+        System.out.println("To-Do List:");
         System.out.println(task.getName());
+        // TODO: Make linked list to store the todo list
     }
 }
