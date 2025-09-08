8d498d99503bbee6ca08d6250bc03aeb5f765970
truesolace <dallasrollyson@icloud.com>
2025-09-07 20:51:43 -0400

    Implemented a push and pop function to the linked list

---
+++ LinkedList.java
@@ -0,0 +1,43 @@
+public class LinkedList {
+    
+    // Constructor
+    public LinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    public void push(Node newNode) {
+        // Empty list
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        // Non-Empty list
+        else {
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+    }
+
+    public void pop(String taskName) {
+        Node pointer = head;
+        // If the desired task is first, move head to next task
+        if (pointer.getTask().getName() == taskName) {
+            head = head.getNext();
+        }
+        // If it is not, traverse the list until it is found, then remove it
+        else {
+            while (pointer != null) {
+                if (pointer.getNext().getTask().getName() == taskName) {
+                    pointer.setNext(pointer.getNext().getNext())
+                }
+                else {
+                    pointer = pointer.getNext();
+                }
+            }
+        }
+    }
+
+
+
+}
