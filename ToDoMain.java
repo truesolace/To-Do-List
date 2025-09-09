2e4450f242fca3c15bbce46ac4872bbfde9e8988
truesolace <dallasrollyson@icloud.com>
2025-09-09 11:44:49 -0400

    Removed old test statements and added the prints for user input to be added next

--- ToDoMain.java
+++ ToDoMain.java
@@ -1,15 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+
+
 class ToDoMain {
 
     public static void main(String[] args) {
-        System.out.println("What would you like to name your task?");
-        String taskName;
+        System.out.println("What would you like do do?");
+        System.out.println("1 = Add Task");
+        System.out.println("2 = Delete Task");
+        System.out.println("3 = Mark task done/not done");
+
+        int userChoice = 4;
         try (Scanner scn = new Scanner(System.in)) {
             taskName = scn.nextLine();
         }
-
-        Task task = new Task(taskName);
-        System.out.println("To-Do List:");
-        System.out.println(task.getName());
         // TODO: Make linked list to store the todo list
     }
 }
